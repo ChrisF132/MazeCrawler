c04a4fbaa79f7f4b2c738491fbb55c4f0c474302
ChrisF132 <152531247+ChrisF132@users.noreply.github.com>
2025-03-02 13:52:11 -0500

    Update Main.java
    Final Version Main

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        char[][] charArray = new char[7][20];
-        Maze maze = new Maze();
 
-        maze.fillMaze(charArray);
+        Maze maze = new Maze("maze.txt");
 
-
-
-
-
-        for(int i = 0; i < charArray.length; i++) {
-            for(int j = 0; j < charArray[i].length; j++) {
-                System.out.print(charArray[i][j]);
-            }
-            System.out.println();
-        }
-
-
+        System.out.println("Pre-solve: ");
+        maze.printMaze();
+        maze.solveMaze(maze.startRow, maze.startCol);
+        System.out.println("Post-solve: ");
+        maze.printMaze();
     }
 }
